78a840e4a5a7cb70dbaea8660a8929adf9284464
Bassam <jaweesh4123@gmail.com>
2023-05-27 19:43:54 +0000

    Update colors.dart

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -5,6 +5,28 @@ Color dark_blue=Color.fromARGB(255, 20, 21, 87);
 Color dark_purple=Color.fromARGB(255, 35, 20, 87);
 Color basic_background=Color.fromARGB(255, 239, 244, 249);
 
+const primaryColor = Color(0xFFE2E2E2);
+const primaryDark = Colors.black;
+
+const primaryColor1 = Color(0xFF7DE5ED);
+const primaryColor2 = Color(0xFF81C6E8);
+const primaryColor3 = Color(0xFF5DA7DB);
+
+const shadow = Color(0xFF013C69);
+
+const iconsColor = Color.fromARGB(255, 0, 40, 72);
+
+const basicColor = Color(0xFF1564BF);
+
+const tealColor = Colors.teal;
+
+const primaryGradient = RadialGradient(
+    radius: 4,
+    colors: [
+      Colors.lightBlue,
+      Colors.blueAccent
+
+    ]);
 
 /*
 const primaryColor = Color(0xFFD6E2EA);
